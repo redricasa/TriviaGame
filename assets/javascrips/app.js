0e0a079d0558b50fb4f20e5da5d12d3df60e3437
redricasa <fredeb26@gmail.com>
2019-08-24 00:32:33 -0700

    added an object of correct answers, added a div for displaying the timer and added code for setting interval and setting timeout, if/else stmt for decrementing timer from 30 seconds, on click event function initiated

--- assets/javascrips/app.js
+++ assets/javascrips/app.js
@@ -1,7 +1,32 @@
 // console.log($); to test if jquery's cdn works
 
 $(document).ready(function(){
-    console.log(this);
+//timer function
+    timer--;
+    
+
+    $("#timer").text("timer:  ").append(timer);
+//set interval of a second
+    var interval = setinterval(function(){
+        console.log("second passed");
+    }, 1000);
+
+//timer of 30 seconds
+    var timer = setTimeout(timeout, 30000);
+    if (timer=0){
+        clearTimeout(timer);
+        console.log("timer went out!")
+    }else{
+        timer--;
+    }
+//when start button gets clicked, the timer starts to decrement    
+    $("#start").on("click", function(){
+        console.log("start button clicked")
+        // timer--;
+    });
+
+
+
 
 
 
