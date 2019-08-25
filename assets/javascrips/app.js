b688c6a266c04da28a2498a92f711bb75d402796
redricasa <fredeb26@gmail.com>
2019-08-24 17:24:06 -0700

    modified app.js to decrement timer from 30 in 1 second increments- instead of setTimer() function, I used setInterval instead. added radio buttons for the options for each question

--- assets/javascrips/app.js
+++ assets/javascrips/app.js
@@ -1,50 +1,27 @@
 // console.log($); to test if jquery's cdn works
 
 $(document).ready(function(){
-//timer function
-    timer--;
-    var timeout= 0;
 
+    var count=30;
     $("#timer").text("timer:  ").append(timer);
-//set interval of a second
-    var interval = setInterval(function(){
-        // console.log("second passed"); 
-    }, 1000);
-
-//timer of 30 seconds
-    var timer = setTimeout(timeout, 30000);
+//set interval of a second 
+    var interval = setInterval(timerFunction, 1000);
+    function timerFunction(){
+        console.log(count--); 
 //if timer gets to 0, stop the timer from going into the negative numbers (clearTimeout)
-    if (timer=0){
-        clearTimeout(timer);
-        console.log("timer went out!");
-    }else{
-        timer--; 
-    }
+        if (count===0){
+            clearInterval(interval);
+            console.log("timer went out!");
+    };  
 //when start button gets clicked, the timer starts to decrement    
     $("#start").on("click", function(){
         console.log("start button clicked")
         // timer--;
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//clearInterval
+//DOM radio button groups
+    //create attributes for answer index
+//submit button
 
 });
 
