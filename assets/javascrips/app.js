77374f263c8bdb9b8f29ac2c3a2301acc050e5d2
redricasa <fredeb26@gmail.com>
2019-08-24 23:29:03 -0700

    timer countdown works after getting rid of the else statement and decrementing with --count.added divs for in/correct displaying amount

--- assets/javascrips/app.js
+++ assets/javascrips/app.js
@@ -1,27 +1,49 @@
 // console.log($); to test if jquery's cdn works
 $(document).ready(function(){
 
-var count=30;
-$("#timer").text("timer: ").append(count);
-//set interval of a second 
-var interval = setInterval(timerFunction, 1000);
-function timerFunction(){
-    console.log(count--); 
-//if timer gets to 0, stop the timer from going into the negative numbers (clearTimeout)
-    if (count===0){
-        clearInterval(interval);
-        console.log("timer went out!");
-    };
-};  
+//set interval of a second  
+var interval;
+function timerOn(){
+    var count=30;
+    interval = setInterval(timerFunction, 1000);
+    function timerFunction(){
+    //if timer gets to 0, stop the timer from going into the negative numbers (clearTimeout)
+        if (count===0){
+            clearInterval(interval);
+            $("#timer").text("Time's up!")
+            return;
+        }
+        --count;
+        $("#timer").text(count);
+    };  
+};
+
 //when start button gets clicked, the timer starts to decrement    
-$("#start").on("click", function(){
-    console.log("start button clicked");
-    count--;        
+$("#start").on("click", function(){ 
+    timerOn();
+    console.log("start button pressed")
 });
 
-//DOM radio button groups
-    //create attributes for answer index
+//create attributes for answer index???
 //submit button
+$("#submit").on("click", function(){
+    if (interval){
+        clearInterval(interval);
+    }
+    
+//display # of in/correct answers
+});
+//a countdown function
+
+// var countDown = function(){
+//     for(var i=0; i < count.length ; i++){
+//        timerFunction();
+//     //    console.log(countDown);
+//     };
+// }
+
+
+
 
 });
 
