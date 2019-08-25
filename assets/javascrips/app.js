fa27c5706c9fe416974027dbeb09b51d074c742e
redricasa <fredeb26@gmail.com>
2019-08-24 18:32:31 -0700

    added all the radio button divs for all the questions. added count-- to on click function of start button

--- assets/javascrips/app.js
+++ assets/javascrips/app.js
@@ -1,23 +1,23 @@
 // console.log($); to test if jquery's cdn works
-
 $(document).ready(function(){
 
-    var count=30;
-    $("#timer").text("timer:  ").append(timer);
+var count=30;
+$("#timer").text("timer: ").append(count);
 //set interval of a second 
-    var interval = setInterval(timerFunction, 1000);
-    function timerFunction(){
-        console.log(count--); 
+var interval = setInterval(timerFunction, 1000);
+function timerFunction(){
+    console.log(count--); 
 //if timer gets to 0, stop the timer from going into the negative numbers (clearTimeout)
-        if (count===0){
-            clearInterval(interval);
-            console.log("timer went out!");
-    };  
+    if (count===0){
+        clearInterval(interval);
+        console.log("timer went out!");
+    };
+};  
 //when start button gets clicked, the timer starts to decrement    
-    $("#start").on("click", function(){
-        console.log("start button clicked")
-        // timer--;
-    });
+$("#start").on("click", function(){
+    console.log("start button clicked");
+    count--;        
+});
 //clearInterval
 //DOM radio button groups
     //create attributes for answer index
