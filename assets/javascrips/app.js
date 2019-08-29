220c1cd78decd05748330c91334993a0bcc98021
redricasa <fredeb26@gmail.com>
2019-08-29 09:43:42 -0700

    got rid of code that wasn't doing anything

--- assets/javascrips/app.js
+++ assets/javascrips/app.js
@@ -1,4 +1,4 @@
-// console.log($); to test if jquery's cdn works
+// console.log($); to test if jquery's CIn works
 $(document).ready(function(){
     var languageBank = [
         { 
@@ -23,25 +23,27 @@ $(document).ready(function(){
         }
     ];
     var questions = $("#questions");
-    for (var index = 0; index < languageBank.length; index++){
+    for (var I = 0; I < languageBank.length; I++){
         var div = $("<div>");
-        //set question lables 'language'
+        //set lables tags with id name == for
 
-        var library = languageBank[index];
+        var library = languageBank[I];
         var language = library['Language']
         var nations = library['countries']
         div.text(language)
-        for (var countryIndex = 0; countryIndex < nations.length; countryIndex++){
+        for (var CI = 0; CI < nations.length; CI++){
+            //creats radio buttons
             var radioButton = $("<input>");
-            var nationname = nations[countryIndex]
-            // set input lables 'countries'
-            radioButton.attr({"type": "radio", "name": index, "value": countryIndex})
-            radioButton.text(nationname)
+            radioButton.attr({"type": "radio", "name": I, "value": CI})
+            //appends the radio buttons created to the divs
             div.append(radioButton)
+            // set input lables 'countries'
+            
         }
+        //adds the divs to the div with the ID of questions
         questions.append(div);
     }
-    //an object/map of the index of questions set to index of correct answers
+    //an object/map of the I of questions set to I of correct answers
     var correctAnswers = { 0:0, 1:0, 2:1, 3:1, 4:0 }
     //set interval of a second  
     var interval;
@@ -65,7 +67,7 @@ $(document).ready(function(){
         //uncheck all radio buttons when start button pressed
         console.log("start button pressed")
     });
-    //create attributes for answer index???
+    //create attributes for answer I???
     //submit button
     $("#submit").on("click", function(){
         var correct = 0; 
@@ -75,19 +77,19 @@ $(document).ready(function(){
             clearInterval(interval);
         };
         //loops through the languageBank array and,
-        for(var i=0; i< languageBank.length; i++ ){
+        for(var I=0; I< languageBank.length; I++ ){
             //elem stores the checked radio button in a group clustered by 'name' attribute
-            var elem =$("input[name="+i+"]:checked");
-            //val stores the value of elem which stores the index of bitton clicked per group of name 
+            var elem =$("input[name="+I+"]:checked");
+            //val stores the value of elem which stores the I of bitton clicked per group of name 
             var val = elem.val();
             console.log(val);
             //the name:value of the radio buttons correspond to language#:country#- the if/else stmt checks to see if correct answers map matches to the name/value map-also displays # of corresponding scores  
-            if(correctAnswers[i]===Number(val)){
+            if(correctAnswers[I]===Number(val)){
                 $("#correct").text(++correct);
-                console.log("correct for the question at index # "+ i);
+                console.log("correct for the question at I # "+ I);
             }else{
                 $("#incorrect").text(++incorrect);
-                console.log("wrong for the question at index # "+ i);
+                console.log("wrong for the question at I # "+ I);
             }
         }         
     });
